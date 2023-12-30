5dc947b187ab33d7673c88acc73e719945c66741
harshitab07 <harshitabhatt1730@gmail.com>
2023-12-30 15:21:17 +0530

    ''

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import 'react-toastify/dist/ReactToastify.css';
+import ForgotPassword from './pages/auth/Forgot';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+    <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='*' element={<Error />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { toast } from "react-toastify";
+import Layout from '../components/layout/layout';
+import axios from 'axios';
+import { useAuth } from '../context/auth';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
+
+  const getAllCategories = async () => {
+    try {
+      const { data } = await axios.get('/api/v1/category/get-categories');
+
+      if (data?.success) {
+        setCategories(data.categories);
+      }
+    } catch (error) {
+      console.log("Error in getting categories", { error });
+      toast.error("Something went wrong in getting category!");
+    }
+  };
+
+  const getAllExpenses = async () => {
+    try {
+        const { data } = await axios.get(`/api/v1/expense/get-expense`);
+        if (data?.success) {
+            setProducts(data.data);
+        } else {
+            toast.error(data?.message);
+        }
+    } catch (error) {
+        console.log('Failed to retrieve all expenses', { error });
+        toast.error('Cannot retrieve expenses at this moment!');
+    }
+}
+
+useEffect(() => {
+  if (auth?.token) {
+    getAllExpenses();
+    getAllCategories();
+  } else {
+    navigate('/login');
+  } 
+}, [auth?.token]);
+
+  return (
+    <Layout title='Home : My-Expense-Tracker'>
+
+    </Layout>
+  )
+}
+
+export default Home

2c9753d60a46bfdf08ac0e044f45b762a5417a85
Harshita Bhatt <95317276+harshitab07@users.noreply.github.com>
2023-12-30 15:25:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './db.js';
+import cors from 'cors';
+import authRoutes from './routes/authRoute.js';
+import expenseRoutes from './routes/expenseRoute.js';
+import categoryRoutes from './routes/categoryRoute.js';
+
+// rest obj
+const app = express();
+
+// config env
+dotenv.config();
+
+// config db
+connectDb();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome to Angular Login/Signup  Page</h1>`);
+} );
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/expense', expenseRoutes);
+app.use('/api/v1/category', categoryRoutes);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server running on mode at port ${port}.`);
+})
